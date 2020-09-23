17c5d61e2cb968bbbac2f008b4fe3c7965df9304
Redsheeep <qwsdcv61215@gmail.com>
2020-09-24 07:14:03 +0800

    1
    1

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const cors = require("cors");
 
 const app = express();
 
+
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://127.0.0.1:8081"
 };
 
 app.use(cors(corsOptions));
@@ -15,6 +16,30 @@ app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+// app.use(function(req, res, next) {  
+//   res.header('Access-Control-Allow-Origin', "*");
+//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });  
 
 const db = require("./app/models");
 db.mongoose
@@ -39,6 +64,7 @@ require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const HOST = 'localhost';
+app.listen(PORT, HOST, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
