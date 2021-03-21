48968421628e65dd78d274c768a9904456b0e84a
Redsheeep <qwsdcv61215@gmail.com>
2021-03-22 04:44:50 +0800

    make it better

--- server.js
+++ server.js
@@ -71,7 +71,8 @@ require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-const HOST = 'localhost';
+// const HOST = 'localhost';
+const HOST = '0.0.0.0';
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
